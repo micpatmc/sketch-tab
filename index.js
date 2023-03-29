51bca126351c92e347806d45ec85d3f5885a386a
Michael McGarvey <71113885+MochaGamesDev@users.noreply.github.com>
2023-03-29 19:12:30 -0400

    Added more background colors

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-var background_colors = ['#4A98F7', '#f74a4a', '#4af778'];
-var background_bottom_colors = ['#1f3c61', '#611f1f', '#1f6129'];
+var background_colors = ['#87bdff', '#ffa0a0', '#b0ffc5', '#fb87ff', '#87f3ff' ];
+var background_bottom_colors = ['#1f3c61', '#611f1f', '#1f6129', '#5e1f61', '#1f5d61'];
 var ran = Math.floor(Math.random() * background_colors.length);
 
-// document.getElementByClassName("background").style.backgroundColor = "blue";
 document.getElementById("background_ID").style.backgroundColor = background_colors[ran];
 document.getElementById("background_bottom_ID").style.backgroundColor = background_bottom_colors[ran];
