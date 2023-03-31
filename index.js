6146ccaf9b9defdc4dc6a7051f1e22b237487550
MochaDev <michaelmcgarvey03@gmail.com>
2023-03-31 09:31:08 -0400

    Implementing formatting

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var background_colors = ["#87bdff", "#ffa0a0", "#b0ffc5", "#fb87ff", "#87f3ff" ]
 var background_bottom_colors = ["#1f3c61", "#611f1f", "#1f6129", "#5e1f61", "#1f5d61"];
 var ran = Math.floor(Math.random() * background_colors.length);
 
-document.getElementById("background_ID").style.backgroundColor = background_colors[ran];
-document.getElementById("background_bottom_ID").style.backgroundColor = background_bottom_colors[ran];
+document.getElementById("background-ID").style.backgroundColor = background_colors[ran];
+
+document.getElementById("background-bottom-ID-1").style.backgroundColor = background_bottom_colors[ran];
+document.getElementById("background-bottom-ID-2").style.backgroundColor = background_bottom_colors[ran];
+document.getElementById("background-bottom-ID-3").style.backgroundColor = background_bottom_colors[ran];
 
 // Draw
 const canvas = document.getElementById("drawing-board");
@@ -153,8 +156,8 @@ saveImg.addEventListener("click", () => {
 });
 
 colorPicker.addEventListener("change", () => {
-    colorPicker.parentElement.style.background = colorPicker.value;
-    colorPicker.parentElement.click();
+    colorPicker.style.background = colorPicker.value;
+    colorPicker.click();
 })
 
 canvas.addEventListener("mousedown", startDraw);
